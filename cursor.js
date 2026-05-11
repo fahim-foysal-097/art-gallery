361c2f3a105b1476c85632c348f34e8e7895c91c
Fahim Foysal <fahimfaysal418@gmail.com>
2026-05-12 01:44:21 +0600

    better cursor

--- cursor.js
+++ cursor.js
@@ -1,10 +1,7 @@
 const cursorDot = document.querySelector("[data-cursor-dot]");
 const cursorOutline = document.querySelector("[data-cursor-outline]");
 
-const rootStyles = getComputedStyle(document.documentElement);
-const accentColor = rootStyles.getPropertyValue("--accent-color").trim();
-
-window.addEventListener("mousemove", function (e) {
+window.addEventListener("mousemove", (e) => {
   const posX = e.clientX;
   const posY = e.clientY;
 
@@ -18,7 +15,7 @@ window.addEventListener("mousemove", function (e) {
       top: `${posY}px`,
     },
     {
-      duration: 500,
+      duration: 180,
       fill: "forwards",
     },
   );
@@ -29,19 +26,10 @@ const interactiveElements = document.querySelectorAll("a, button");
 
 interactiveElements.forEach((el) => {
   el.addEventListener("mouseenter", () => {
-    cursorOutline.style.transform = "translate(-50%, -50%) scale(1.5)";
-
-    cursorOutline.style.backgroundColor = `${accentColor}22`;
-    // 22 = low alpha transparency
-
-    cursorOutline.style.borderColor = accentColor;
+    cursorOutline.classList.add("hover");
   });
 
   el.addEventListener("mouseleave", () => {
-    cursorOutline.style.transform = "translate(-50%, -50%) scale(1)";
-
-    cursorOutline.style.backgroundColor = "transparent";
-
-    cursorOutline.style.borderColor = accentColor;
+    cursorOutline.classList.remove("hover");
   });
 });
